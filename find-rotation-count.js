d96c3c49370c014b4503964ebf0186202777d41d
bc-dipesh <bcdipeshwork@gmail.com>
2023-05-30 17:35:20 -0400

    Implement find-rotation-count function

--- find-rotation-count.js
+++ find-rotation-count.js
@@ -1,5 +1,30 @@
-function findRotationCount() {
-  
+function findRotationCount(arr) {
+	let pivotIndex = findPivot(arr);
+	return pivotIndex;
 }
 
-module.exports = findRotationCount
+function findPivot(arr, low = 0, high = arr.length - 1) {
+	if (high < low) {
+		return 0;
+	}
+	if (high === low) {
+		return low;
+	}
+
+	let mid = Math.floor((low + high) / 2);
+
+	if (mid < high && arr[mid] > arr[mid + 1]) {
+		return mid + 1;
+	}
+	if (mid > low && arr[mid] < arr[mid - 1]) {
+		return mid;
+	}
+
+	if (arr[low] >= arr[mid]) {
+		return findPivot(arr, low, mid - 1);
+	} else {
+		return findPivot(arr, mid + 1, high);
+	}
+}
+
+module.exports = findRotationCount;

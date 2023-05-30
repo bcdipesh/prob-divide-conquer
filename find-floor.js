dd236bbe32b1f2e96e246d2248f2296b4c4c74f7
bc-dipesh <bcdipeshwork@gmail.com>
2023-05-30 17:36:36 -0400

    Implement find-floor function

--- find-floor.js
+++ find-floor.js
@@ -1,5 +1,24 @@
-function findFloor() {
-  
+function findFloor(arr, x) {
+	let low = 0;
+	let high = arr.length - 1;
+
+	if (x < arr[low]) {
+		return -1;
+	}
+
+	while (low <= high) {
+		let mid = Math.floor((low + high) / 2);
+
+		if (arr[mid] === x) {
+			return arr[mid];
+		} else if (arr[mid] < x) {
+			low = mid + 1;
+		} else {
+			high = mid - 1;
+		}
+	}
+
+	return arr[high];
 }
 
-module.exports = findFloor
+module.exports = findFloor;
